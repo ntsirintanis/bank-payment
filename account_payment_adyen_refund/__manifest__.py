92b48a0706080bd94e83fb26abf30d7c91ed9f83
Ronald Portier (Therp BV) <ronald@therp.nl>
2023-07-03 13:38:32 +0200

    [ADD] new module: account_payment_adyen_refund

---
+++ account_payment_adyen_refund/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Therp BV <https://therp.nl>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Account Payment Adyen Refund",
+    "version": "13.0.1.0.0",
+    "category": "Banking addons",
+    "license": "AGPL-3",
+    "summary": "Adds method to refund payments made through Adyen",
+    "author": "Therp BV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "depends": ["payment_adyen_paybylink"],
+    "data": [
+        "data/account_payment_method.xml",
+        "views/account_invoice.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ account_payment_adyen_refund/models/account_move.py
@@ -0,0 +1,32 @@
+# Copyright - 2023 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+"""Support Adyen Refund."""
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    """Support Adyen Refund."""
+    _inherit = "account.move"
+
+    # This field is identical to the one in base_transaction_id,
+    # but I do not want to add a dependency, but neither do
+    # I want to have a field with the same purpose and another name.
+    transaction_id = fields.Char(
+        string="Transaction ID",
+        index=True,
+        copy=False,
+        help="Transaction ID from the " "financial institute",
+    )
+    # This field will be used to keep track of Adyen refund status, but
+    # conceivable might also be usefull for other payment types.
+    refund_status = fields.Selection(
+        selection=[
+            ("none", "No refund submitted"),
+            ("submitted", "Requested from payment provider"),
+            ("done", "Confirmed by payment provider"),
+            ("failed", "Payment provider rejected payment"),
+        ],
+        default="none",
+        readonly=True,
+    )
+    refund_failure_reason = fields.Text(readonly=True)
